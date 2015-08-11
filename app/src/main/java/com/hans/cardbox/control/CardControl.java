6f1ec759d9ecad7e1c9a9966f5c83ce90631e3d3
Hanbo <hanbov5@gmail.com>
2015-08-11 21:26:54 +0800

    no message

--- app/src/main/java/com/hans/cardbox/control/CardControl.java
+++ app/src/main/java/com/hans/cardbox/control/CardControl.java
@@ -16,17 +16,20 @@ import com.hans.cardbox.tools.UriTools;
 import com.hans.mydb.in.DD;
 import com.hans.mydb.model.SeLectInfo;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import cn.bmob.v3.BmobObject;
 import cn.bmob.v3.listener.SaveListener;
+import cn.bmob.v3.listener.UpdateListener;
 
 /**
  * Created by hanbo1 on 2015/8/10.
  */
 public class CardControl {
 
-    public static void addCard(final Card card,MessageListener listener){
-        if(TextUtils.isEmpty(card.cardID)){
+    public static void addCard(final Card card, MessageListener listener) {
+        if (TextUtils.isEmpty(card.cardID)) {
             throw new RuntimeException("主键未设置啊");
         }
         BackThreadPool.post(new BackRunnable(listener) {
@@ -60,9 +63,9 @@ public class CardControl {
      * @param cardID
      * @return
      */
-    private static Card getCard(String cardID){
-        if(TextUtils.isEmpty(cardID)){
-           return null;
+    private static Card getCard(String cardID) {
+        if (TextUtils.isEmpty(cardID)) {
+            return null;
         }
         Card card = null;
         try {
@@ -71,23 +74,32 @@ public class CardControl {
             e.printStackTrace();
             return card;
         }
-        List<KeyPass> kps = DD.get(KeyPass.class, SeLectInfo.columnIs(new String[]{Card.getPKColumn()},new String[]{cardID}));
-        List<QuickKey> qks = DD.get(QuickKey.class, SeLectInfo.columnIs(new String[]{Card.getPKColumn()},new String[]{cardID}));
+        List<KeyPass> kps = DD.get(KeyPass.class, SeLectInfo.columnIs(new String[]{Card.getPKColumn()}, new String[]{cardID}));
+        List<QuickKey> qks = DD.get(QuickKey.class, SeLectInfo.columnIs(new String[]{Card.getPKColumn()}, new String[]{cardID}));
         card.kkps(kps);
         card.quicyKeys(qks);
         return card;
     }
 
-       public static void syncCard(final MessageListener listener){
+    public static void syncCard(final MessageListener listener) {
         BackThreadPool.post(new BackRunnable(listener) {
             @Override
             public void run() {
                 List<LocalCardChange> allChanged = DD.get(LocalCardChange.class, null);
-                notifyMsgIfAvailable(UriTools.getPromptUri("同步数量："+allChanged.size()));
-                for (int i=0,size  = allChanged.size();i<size;i++){
+                notifyMsgIfAvailable(UriTools.getPromptUri("同步数量：" + allChanged.size()));
+                List<BmobObject> inserts = new ArrayList<>();
+                List<BmobObject> updates = new ArrayList<>();
+                for (int i = 0, size = allChanged.size(); i < size; i++) {
                     LocalCardChange lc = allChanged.get(i);
                     Card card = getCard(lc.carID);
-                    card.save(App.mApp, new SaveListener() {
+                    if (TextUtils.isEmpty(card.bmobID)) {
+                        inserts.add(card);
+                    } else {
+                        updates.add(card);
+                    }
+                }
+                if (!TT.isEmpty(inserts)) {
+                    new BmobObject().insertBatch(App.mApp, inserts, new SaveListener() {
                         @Override
                         public void onSuccess() {
 
@@ -99,22 +111,20 @@ public class CardControl {
                         }
                     });
                 }
-                List<KeyPass> kps = card.kps;
-                if (!TT.isEmpty(kps)) {
-                    DD.saveaLot(kps);
+                if (!TT.isEmpty(updates)) {
+                    new BmobObject().updateBatch(App.mApp, updates, new UpdateListener() {
+                        @Override
+                        public void onSuccess() {
+                        }
+
+                        @Override
+                        public void onFailure(int code, String msg) {
+                        }
+                    });
                 }
-                LocalCardChange lc = new LocalCardChange(Card.class.getName(), DD.getPK(Card.class).getColumn());
-                DD.saveSingle(lc);
-                Uri uri = UriTools.getAsyncMsgUri(true,"添加成功",null);
-                notifyMsg(uri);
             }
         }, null);
     }
-
-
-
-    public 
-
 
 
 }
